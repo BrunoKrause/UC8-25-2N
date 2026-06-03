b37618f21f39c7cd093be77c860f06004f92bd1f
BRUNO KRAUSE NOIA DA COSTA <02155268050@senacrs.edu.br>
2026-06-02 21:47:26 -0300

    Att

--- NetBeansProjects/NewAPP/src/telaPrincipal.java
+++ NetBeansProjects/NewAPP/src/telaPrincipal.java
@@ -23,7 +23,7 @@ public class telaPrincipal extends javax.swing.JFrame {
         btnCadCliente = new javax.swing.JButton();
         btnCadProduto = new javax.swing.JButton();
         btnCadServiço = new javax.swing.JButton();
-        jDesktopPane2 = new javax.swing.JDesktopPane();
+        desktopPrincipal = new javax.swing.JDesktopPane();
         jToolBar1 = new javax.swing.JToolBar();
         jMenuBar1 = new javax.swing.JMenuBar();
         menuCadastro = new javax.swing.JMenu();
@@ -38,6 +38,10 @@ public class telaPrincipal extends javax.swing.JFrame {
         itemRelatorioCliente = new javax.swing.JMenuItem();
         itemRelatorioProduto = new javax.swing.JMenuItem();
         itemRelatorioServiço = new javax.swing.JMenuItem();
+        menuUtilitário = new javax.swing.JMenu();
+        menuSobre = new javax.swing.JMenu();
+        menuAjuda = new javax.swing.JMenu();
+        menuJanela = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,12 +57,27 @@ public class telaPrincipal extends javax.swing.JFrame {
 
         btnCadCliente.setIcon(new javax.swing.ImageIcon("C:\\Users\\BRUNOKRAUSENOIADACOS\\Downloads\\Icones\\32 x 32\\cliente(1).png")); // NOI18N
         btnCadCliente.setToolTipText("Cadastro Cliente");
+        btnCadCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCadClienteActionPerformed(evt);
+            }
+        });
 
         btnCadProduto.setIcon(new javax.swing.ImageIcon("C:\\Users\\BRUNOKRAUSENOIADACOS\\Downloads\\Icones\\32 x 32\\descricao-do-produto.png")); // NOI18N
         btnCadProduto.setToolTipText("Cadastro Produto");
+        btnCadProduto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCadProdutoActionPerformed(evt);
+            }
+        });
 
         btnCadServiço.setIcon(new javax.swing.ImageIcon("C:\\Users\\BRUNOKRAUSENOIADACOS\\Downloads\\Icones\\32 x 32\\ordem-de-servico.png")); // NOI18N
         btnCadServiço.setToolTipText("Cadastro Serviço");
+        btnCadServiço.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCadServiçoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -86,22 +105,22 @@ public class telaPrincipal extends javax.swing.JFrame {
                 .addGap(0, 0, Short.MAX_VALUE))
         );
 
-        jDesktopPane2.setBackground(new java.awt.Color(204, 204, 204));
+        desktopPrincipal.setBackground(new java.awt.Color(204, 204, 204));
 
         jToolBar1.setBackground(new java.awt.Color(51, 51, 51));
         jToolBar1.setRollover(true);
 
-        jDesktopPane2.setLayer(jToolBar1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        desktopPrincipal.setLayer(jToolBar1, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
-        javax.swing.GroupLayout jDesktopPane2Layout = new javax.swing.GroupLayout(jDesktopPane2);
-        jDesktopPane2.setLayout(jDesktopPane2Layout);
-        jDesktopPane2Layout.setHorizontalGroup(
-            jDesktopPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout desktopPrincipalLayout = new javax.swing.GroupLayout(desktopPrincipal);
+        desktopPrincipal.setLayout(desktopPrincipalLayout);
+        desktopPrincipalLayout.setHorizontalGroup(
+            desktopPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-        jDesktopPane2Layout.setVerticalGroup(
-            jDesktopPane2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDesktopPane2Layout.createSequentialGroup()
+        desktopPrincipalLayout.setVerticalGroup(
+            desktopPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, desktopPrincipalLayout.createSequentialGroup()
                 .addGap(0, 226, Short.MAX_VALUE)
                 .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -109,12 +128,27 @@ public class telaPrincipal extends javax.swing.JFrame {
         menuCadastro.setText("Cadastro");
 
         itemCadastroCliente.setText("Cliente");
+        itemCadastroCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemCadastroClienteActionPerformed(evt);
+            }
+        });
         menuCadastro.add(itemCadastroCliente);
 
         itemCadastroProduto.setText("Produto");
+        itemCadastroProduto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemCadastroProdutoActionPerformed(evt);
+            }
+        });
         menuCadastro.add(itemCadastroProduto);
 
         itemCadastroServiço.setText("Serviço");
+        itemCadastroServiço.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemCadastroServiçoActionPerformed(evt);
+            }
+        });
         menuCadastro.add(itemCadastroServiço);
 
         jMenuBar1.add(menuCadastro);
@@ -145,6 +179,18 @@ public class telaPrincipal extends javax.swing.JFrame {
 
         jMenuBar1.add(menuRelatório);
 
+        menuUtilitário.setText("Utilitário");
+        jMenuBar1.add(menuUtilitário);
+
+        menuSobre.setText("Sobre");
+        jMenuBar1.add(menuSobre);
+
+        menuAjuda.setText("Ajuda");
+        jMenuBar1.add(menuAjuda);
+
+        menuJanela.setText("Janela");
+        jMenuBar1.add(menuJanela);
+
         setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -152,14 +198,14 @@ public class telaPrincipal extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jDesktopPane2)
+            .addComponent(desktopPrincipal)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jDesktopPane2))
+                .addComponent(desktopPrincipal))
         );
 
         pack();
@@ -170,6 +216,48 @@ public class telaPrincipal extends javax.swing.JFrame {
         setVisible(false);
         login.setVisible(true);
     }//GEN-LAST:event_btnSairActionPerformed
+
+    private void itemCadastroClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemCadastroClienteActionPerformed
+        telaCadCliente cadcliente = new telaCadCliente();
+        desktopPrincipal.add(cadcliente);
+        cadcliente.setVisible(true);
+        cadcliente.setLocation(50,50);
+    }//GEN-LAST:event_itemCadastroClienteActionPerformed
+
+    private void itemCadastroProdutoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemCadastroProdutoActionPerformed
+        telaCadProduto tela = new telaCadProduto();
+        desktopPrincipal.add(tela);
+        tela.setVisible(true);
+        tela.setLocation(50,50);
+    }//GEN-LAST:event_itemCadastroProdutoActionPerformed
+
+    private void itemCadastroServiçoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemCadastroServiçoActionPerformed
+        telaCadServiço tela = new telaCadServiço();
+        desktopPrincipal.add(tela);
+        tela.setVisible(true);
+        tela.setLocation(50,50);
+    }//GEN-LAST:event_itemCadastroServiçoActionPerformed
+
+    private void btnCadClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadClienteActionPerformed
+        telaCadCliente cadcliente = new telaCadCliente();
+        desktopPrincipal.add(cadcliente);
+        cadcliente.setVisible(true);
+        cadcliente.setLocation(50,50);
+    }//GEN-LAST:event_btnCadClienteActionPerformed
+
+    private void btnCadProdutoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadProdutoActionPerformed
+        telaCadProduto tela = new telaCadProduto();
+        desktopPrincipal.add(tela);
+        tela.setVisible(true);
+        tela.setLocation(50,50);
+    }//GEN-LAST:event_btnCadProdutoActionPerformed
+
+    private void btnCadServiçoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCadServiçoActionPerformed
+        telaCadServiço tela = new telaCadServiço();
+        desktopPrincipal.add(tela);
+        tela.setVisible(true);
+        tela.setLocation(50,50);
+    }//GEN-LAST:event_btnCadServiçoActionPerformed
 
     public static void main(String args[]) {
       
@@ -185,6 +273,7 @@ public class telaPrincipal extends javax.swing.JFrame {
     private javax.swing.JButton btnCadProduto;
     private javax.swing.JButton btnCadServiço;
     private javax.swing.JButton btnSair;
+    private javax.swing.JDesktopPane desktopPrincipal;
     private javax.swing.JMenuItem itemCadastroCliente;
     private javax.swing.JMenuItem itemCadastroProduto;
     private javax.swing.JMenuItem itemCadastroServiço;
@@ -194,12 +283,15 @@ public class telaPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem itemRelatorioCliente;
     private javax.swing.JMenuItem itemRelatorioProduto;
     private javax.swing.JMenuItem itemRelatorioServiço;
-    private javax.swing.JDesktopPane jDesktopPane2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JMenu menuAjuda;
     private javax.swing.JMenu menuCadastro;
     private javax.swing.JMenu menuConsulta;
+    private javax.swing.JMenu menuJanela;
     private javax.swing.JMenu menuRelatório;
+    private javax.swing.JMenu menuSobre;
+    private javax.swing.JMenu menuUtilitário;
     // End of variables declaration//GEN-END:variables
 }
